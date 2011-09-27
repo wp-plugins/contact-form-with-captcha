f1137832ef3944a386032d2042b46e08ff18c0e8
zackhasit <zackhasit@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-27 15:08:44 +0000

    First version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-with-captcha/trunk@444263 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ cfwc-form.php
@@ -0,0 +1,131 @@
+<?php
+include(WP_PLUGIN_DIR . '/contact-form-with-captcha/captcha/recaptchalib.php');
+// Get a key from https://www.google.com/recaptcha/admin/create
+
+# the response from reCAPTCHA
+$resp = null;
+# the error code from reCAPTCHA, if any
+$error = null;
+
+# was there a reCAPTCHA response?
+if ($_POST["recaptcha_response_field"]) {
+        $resp = recaptcha_check_answer ($privatekey,
+                                        $_SERVER["REMOTE_ADDR"],
+                                        $_POST["recaptcha_challenge_field"],
+                                        $_POST["recaptcha_response_field"]);
+
+        if ($resp->is_valid) {
+                
+                {   
+                          
+                    // --- CONFIG PARAMETERS --- //
+                    $email_recipient    = $cfwc_to;
+                    $email_sender       = $_POST["contact_name"];
+                    $email_return_to    = $_POST["contact_email"];
+                    $email_content_type = "text/html; charset=us-ascii";
+                    $email_client       = "PHP/" . phpversion();
+
+                    // --- DEFINE HEADERS --- //
+
+                    $email_header  = "From:         " . $email_sender . "\r\n";
+                    $email_header .= "Reply-To:     " . $email_return_to . "\r\n";
+                    $email_header .= "Return-Path:  " . $email_return_to . "\r\n";
+                    $email_header .= "Content-type: " . $email_content_type . "\r\n";
+                    $email_header .= "X-Mailer:     " . $email_client . "\r\n";
+
+                    // --- SUBJECT AND CONTENTS --- //
+
+                    $email_subject = '============= ' . $_POST["contact_subject"] . ' =============' ;
+                    $email_contents = "<html>";
+                    $email_contents .= "<h2>"                        . $_POST["contact_subject"] . "</h2>";
+                    $email_contents .= "<br><b>Sender:</b>         "         . $email_sender;
+                    $email_contents .= "<br><b>Sender Address:</b> " . $_SERVER["REMOTE_ADDR"];
+                    $email_contents .= "<br><br>" . $_POST["contact_message"];    
+                    $email_contents .= "</html>";
+ 
+                    if (mail($email_recipient, $email_subject, $email_contents, $email_header))
+                    {      
+                        echo "<center><h2>Thank you for contacting us!</h2></center>";
+                    }       
+                    else 
+                    {      
+                        echo "<center><h2>Can't send email to Administrator. Please try later</h2></center>";      
+                    } 
+                }
+        } 
+        else 
+        {
+                # set the error code so that we can display it
+                $error = $resp->error;
+                echo "<center><h2>Incorrect Captcha!</h2></center>";
+        }
+}
+
+
+
+?>
+
+<script language="JavaScript" type="text/javascript">
+
+function focuson() { document.cfwc_contactform.number.focus()}
+
+function check(){
+var str1 = document.getElementById("contact_email").value;
+var filter=/^(.+)@(.+).(.+)$/i
+if (!( filter.test( str1 ))){alert("Incorrect email address!");return false;}
+if(document.getElementById("recaptcha_response_field").value=="")
+   {
+       alert("Please enter captcha");
+       return false;
+   }
+}
+</script>
+
+<? echo '<link rel="stylesheet" type="text/css" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/contact-form-with-captcha/cfwc.css" />';  ?>
+
+<div id="cfwc_contactform">
+<form action="" method="POST" name="ContactForm" onsubmit="return check();">
+
+<table class="cfwc_table">
+       <tbody>
+         <tr>
+             <td class="cfwc_cleanformdata cfwc_formdata">Full Name:</td>
+             <td class="cfwc_cleanformdata"><input name="contact_name" class="cfwc_inputdata" type="text"></td>
+         </tr>
+         <tr><td class="cfwc_cleanformdata">&nbsp;&nbsp;</td></tr>
+         <tr>
+             <td class="cfwc_cleanformdata cfwc_formdata">E-mail:</td>
+             <td class="cfwc_cleanformdata"><input id="contact_email" name="contact_email" class="cfwc_inputdata" type="text"></td>
+         </tr>
+         <tr><td class="cfwc_cleanformdata" >&nbsp;&nbsp;</td></tr>
+         <tr>
+             <td class="cfwc_cleanformdata cfwc_formdata">Subject:</td>
+             <td class="cfwc_cleanformdata"><input name="contact_subject" class="cfwc_inputdata" type="text"></td>
+         </tr>
+         <tr><td class="cfwc_cleanformdata" >&nbsp;&nbsp;</td></tr>
+         <tr>
+             <td class="cfwc_cleanformdata cfwc_formdata">Message: </td>
+             <td class="cfwc_cleanformdata"><a name="s" id="s"></a><textarea name="contact_message" id="contact_message" class="cfwc_textdata"></textarea></td>
+         </tr>
+         <tr><td class="cfwc_cleanformdata">&nbsp;&nbsp;</td></tr>       
+         <tr>
+            <td class="cfwc_cleanformdata"></td>
+            <td class="cfwc_cleanformdata">
+         <?
+            echo recaptcha_get_html($publickey, $error);
+         ?>
+            </td>
+         </tr>
+         <tr><td class="cfwc_cleanformdata">&nbsp;&nbsp;</td></tr>
+         <tr>
+            <td class="cfwc_cleanformdata"></td>
+            <td class="cfwc_cleanformdata">
+             <input name="Contact_Send" class="cfwc_inputdata" value="SendMessage" type="submit">
+             <input name="SendMessage" value="1" type="hidden">
+            </td>
+         </tr>
+     </tbody>
+</table>
+
+</form>
+</div>
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Contact Form With Captcha (CFWC)===
+Contributors: zackhasit
+Donate link: http://www.teknocrat.com/
+Tags: recaptcha form, recaptcha contact form, contact, form, contact form, recaptcha, antispam, captcha
+Requires at least: 3.0
+Tested up to: 3.2
+Stable tag: 1.3
+
+This wordpress plugin allows you to use a Google reCAPTCHA contact form on your blog. It requires PHP 5.0 or later.
+
+== Description ==
+
+A simple plugin for your WordPress blog that enables you to have a contact form with the reCAPTCHA challenge system.
+
+All you have to do is install and activate the plugin, enter your reCAPTCHA keys in the Admin section, and place the 
+shortcode `[cfwc]` on any page or post within your blog.
+
+The form asks users for their name, email address, subject and message.  The plugin will validate email address before sending 
+the email.
+
+Related Links:
+
+* <a href="http://www.teknocrat.com/wordpress-contact-form-with-captcha-plugin.html" title="Wordpress contact form with captcha plugin">Plugin Homepage</a>
+
+
+== Installation ==
+
+1. Upload the `contact-form-with-captcha` directory to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Go to the `Contact Form With Captcha` Admin section and enter your reCAPTCHA private and public keys, and recipient email address
+1. Use the shortcode `[cfwc]` in any of your posts or pages
+
+== ChangeLog ==
+
+= Version 1.0 =
+
+* First release.
+
+== Frequently Asked Questions ==
+
+= Where Can I Use The reCAPTCHA Form? =
+
+You can use it in any post or page by inserting the shortcode `[cfwc]`
+
+= I Don't Have Any reCAPTCHA Keys.  Where Can I Get Them From? =
+
+You can get them from the Google reCAPTCHA website(http://www.google.com/recaptcha/whyrecaptcha "Google reCAPTCHA website").
+
+= Can I Use This Plugin With Other Plugins That Use reCAPTCHA? =
+
+reCAPTCHA Form can be installed alongside any other plugins that use the reCAPTCHA challenge system, however you must 
+make sure that whatever post/page you insert the reCAPTCHA Form shortcode on does not already display a reCAPTCHA 
+form - this is because you cannot display more than one reCAPTCHA form on the same web page.
+
+== Screenshots ==
+
+1. The Contact Form With Captcha
